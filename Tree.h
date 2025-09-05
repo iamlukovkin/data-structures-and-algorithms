e8cacbaa562a74359a7dbb33a22f4704ed76029b
iamlu <iamlukovkin@gmail.com>
2025-09-05 14:20:17 +0300

    add abstract tree

---
+++ Tree.h
@@ -0,0 +1,21 @@
+#ifndef DATA_STRUCTURES_AND_ALGORITHMS_TREE_H
+#define DATA_STRUCTURES_AND_ALGORITHMS_TREE_H
+
+template <typename DataType>
+class Tree {
+public:
+    int getLeftChild(int indexOf) {
+        // TODO: implement
+        return 0;
+    }
+    int getRightChild(int indexOf) {
+        // TODO: implement
+        return 0;
+    }
+    int getParent(int indexOf) {
+        // TODO: implement
+        return 0;
+    }
+};
+
+#endif
